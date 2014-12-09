9983010c95f0aa60d031e801bfd8b88a84a12ef5
Mike Hewitson <mike@hewitson.co.za>
2014-12-09 06:01:33 +0200

    Closing off the course

---
+++ source/ex7.rb
@@ -0,0 +1,41 @@
+
+# puts string
+puts "Mary had a little lamb."
+# puts string with emmbeded block containing string
+puts "It's fleece was white as #{'snow'}."
+# puts string
+puts "And everywhere that Mary went."
+# puts repeated full stop
+puts "." * 10
+
+# assignemtn chacater to var
+end1 = "C"
+# assignemtn chacater to var
+end2 = "h"
+# assignemtn chacater to var
+end3 = "e"
+# assignemtn chacater to var
+end4 = "e"
+# assignemtn chacater to var
+end5 = "s"
+# assignemtn chacater to var
+end6 = "e"
+# assignemtn chacater to var
+# assignemtn chacater to var
+end7 = "B"
+# assignemtn chacater to var
+end8 = "u"
+# assignemtn chacater to var
+end9 = "r"
+# assignemtn chacater to var
+end10 = "g"
+# assignemtn chacater to var
+end11 = "e"
+# assignemtn chacater to var
+end12 = "r"
+
+
+# print string sum of variables
+print end1 + end2 + end3 + end4 + end5 + end6
+# puts string sum of variables
+puts end7 + end8 + end9 + end10 + end11 + end12
